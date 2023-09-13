8c9c4722225a7d1cd687be837cb322c2910d8723
PrerakPithadiya <139850989+PrerakPithadiya@users.noreply.github.com>
2023-09-13 23:51:10 +0530

    Add files via upload

---
+++ 6_Input_Values.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int a, b, c;
+    printf("Enter First Value : ");
+    scanf("%d", &a);
+    printf("Enter Second Value : ");
+    scanf("%d", &b);
+    printf("Enter Third Value : ");
+    scanf("%d", &c);
+    printf("\nInputed Values are : [%d, %d, %d]", a, b, c);
+    return 0;
+}
