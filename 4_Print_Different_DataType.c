3e63eed2eb79b4fea7b5ece07eb15000b2cecec2
PrerakPithadiya <139850989+PrerakPithadiya@users.noreply.github.com>
2023-09-24 05:49:34 +0530

    Add files via upload

---
+++ 4_Print_Different_DataType.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a = 1;
+    float b = 3.1415;
+    char ch = 'P';
+    printf("Integer : %d\n", a);
+    printf("Decimal : %.4f\n", b);
+    printf("Character : %c\n", ch);
+    return 0;
+}
---
+++ 5_Size_of_DataTypes.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int a;
+    float b;
+    char c;
+    double d;
+    printf("Size of 'int' : %d\n", sizeof(a));
+    printf("Size of 'float' : %d\n", sizeof(b));
+    printf("Size of 'char' : %d\n", sizeof(c));
+    printf("Size of 'double' : %d\n", sizeof(d));
+    return 0;
+}
